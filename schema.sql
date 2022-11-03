e658c89885a5c8219092ca265abd315ca7c73c76
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2022-11-03 17:21:49 -0300

    finish all tasks

--- schema.sql
+++ schema.sql
@@ -11,19 +11,27 @@ CREATE TABLE animals (
 );
 
 /* wednesday */
+/* New table for owners */
 CREATE TABLE owners (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	full_name VARCHAR(100) NOT NULL,
 	age INTEGER NOT NULL
 );
 
+/*New table for species */
 CREATE TABLE species (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(100) NOT NULL
 );
 
-/* wednesday */
-
-alter table animals drop column species; /*DELETE COLUMN species*/
-alter table animals add column species_id INT NULL REFERENCES species(id) ON DELETE CASCADE; /*ADD COLUMN species_id, which is a foreign key referencing species table*/
-alter table animals add column owner_id INT NULL REFERENCES owners(id) ON DELETE CASCADE; /*ADD COLUMN owner_id, which is a foreign key referencing owners table*/
+/*Modify table */
+CREATE TABLE animals (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered BOOLEAN NOT NULL,
+  weight_kg DOUBLE PRECISION NOT NULL
+  species_id INT NULL REFERENCES species(id) ON DELETE CASCADE,
+  owner_id INT NULL REFERENCES owners(id) ON DELETE CASCADE,
+);
