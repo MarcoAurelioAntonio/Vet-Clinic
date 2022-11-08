7c0f3c0744a10bb6ffb05f4c0aa9ef1236cd2745
Peter1907 <peter.nady.1408@gmail.com>
2022-11-08 23:36:45 +0200

    Add the remaining tables and relations

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -17,3 +17,24 @@ CREATE TABLE treatments (
     name VARCHAR(100) NOT NULL
 )
 
+CREATE TABLE invoices (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id INT REFERENCES medical_histories(id) ON DELETE CASCADE
+)
+
+CREATE TABLE invoice_items (
+    id BIGSERIAL NOT NULL PRIMARY kEY,
+    unit_price decimal,
+    quantity INT,
+    total_price decimal,
+    invoice_id INT REFERENCES invoices(id) ON DELETE CASCADE,
+    treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE
+)
+
+CREATE TABLE patient_file (
+    history_id INT REFERENCES medical_histories(id),
+    treatment_id INT REFERENCES treatments(id)
+)
